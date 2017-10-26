1863fba78a4b50e78e605649d7e0a2f43a480eb3
wassimkhoury <wassimkh@hotmail.com>
2017-10-26 11:39:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hp1/parkeasier/CameraActivity.java
@@ -0,0 +1,115 @@
+package com.example.hp1.parkeasier;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CameraActivity extends Activity implements OnClickListener {
+    private Bitmap bitmap;
+    private ImageView imageView;
+    Button btCamera,btGalery;
+    static final int SELECT_IMAGE=1;
+    static final int TAKE_IMAGE=0;
+    SharedPreferences preferences = getSharedPreferences("profile",MODE_PRIVATE);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        imageView = (ImageView) findViewById(R.id.imgPhoto);
+
+        btCamera =(Button) findViewById(R.id.btCamera);
+        btGalery = (Button) findViewById(R.id.btGalery);
+        btCamera.setOnClickListener(this);
+        btGalery.setOnClickListener(this);
+
+
+
+        if(!hasCamera()){
+            btCamera.setEnabled(false);
+        }
+    }
+    private boolean hasCamera(){
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+    @Override
+    public void onClick(View v) {
+        if(v==btCamera){
+//start another activity and receive a result back in case the activity exists return 0
+            Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(i,TAKE_IMAGE );
+        }else{
+            Intent i = new Intent(Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(i, SELECT_IMAGE);
+        }
+    }
+    /**
+     *  The resultCode will be RESULT_CANCELED if the activity explicitly returned that, didn't return any result, or crashed during its operation.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == TAKE_IMAGE && resultCode == RESULT_OK){
+            Bundle extra = data.getExtras();
+            bitmap  = (Bitmap) extra.get("data");
+            imageView.setImageBitmap(bitmap);
+            File root = Environment.getExternalStorageDirectory();
+            File file = new File(root.getAbsolutePath()+"/DCIM/Camera/img.jpg");
+            try
+            {
+                file.createNewFile();
+                FileOutputStream ostream = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, ostream);
+                ostream.close();
+            }
+            catch (IOException e)
+            {
+
+            }
+        }
+        else if(requestCode == SELECT_IMAGE && resultCode == RESULT_OK)
+        {
+            Uri targetUri = data.getData();
+            Toast.makeText(getApplicationContext(), targetUri.toString(), 1000).show();
+            //textTargetUri.setText(targetUri.toString());
+            Bitmap bitmap;
+            try {
+
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+/*String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+Cursor cursor = getContentResolver().query(data.getData(), filePathColumn, null, null, null);
+cursor.moveToFirst();
+int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+String filePath = cursor.getString(columnIndex);
+cursor.close();
+
+Bitmap yourSelectedImage = BitmapFactory.decodeFile(filePath);
+Toast.makeText(getApplicationContext(), filePath, 1000).show();
+imageView.setImageBitmap(yourSelectedImage);
+*/
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/hp1/parkeasier/Floor2.java
@@ -0,0 +1,183 @@
+package com.example.hp1.parkeasier;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class Floor2 extends AppCompatActivity implements View.OnClickListener {
+
+    Button[] btn = new Button[12];
+    int[] ids = {R.id.BT1, R.id.BT2, R.id.BT3, R.id.BT4, R.id.BT5, R.id.BT6, R.id.BT7, R.id.BT8, R.id.BT9, R.id.BT10, R.id.BT11, R.id.BT12};
+
+    boolean[] state = {false, false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};
+
+    Button[] btnp = new Button[2];
+    int[] idsp ={R.id.BTP1, R.id.BTP2};
+
+    Button[] btnb = new Button[2];
+    int[] idsb ={R.id.BTB1, R.id.BTB2};
+
+    Spinner spCoffee1;
+    Spinner spCoffee2;
+    Spinner spCoffee3;
+
+    Adapter adapter3;
+    Adapter adapter2;
+    Adapter adapter1;
+
+    String[] spList = {"None", "disablity", "pregnant"};
+    String[] spList2 = {"None", "Gym", "shopping",};
+    String[] spList3 = {"floor2", "floor1"};
+
+    boolean flag1=false;
+
+    Intent[] intents;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        for (int i = 0; i < btn.length; i++) {
+            btn[i] = (Button) findViewById(ids[i]);
+            btn[i].setOnClickListener(this);
+        }
+        for (int i = 0; i < btnp.length; i++) {
+            btnp[i] = (Button) findViewById(idsp[i]);
+            btnp[i].setOnClickListener(this);
+        }
+        for (int i = 0; i < btnb.length; i++) {
+            btnb[i] = (Button) findViewById(idsb[i]);
+            btnb[i].setOnClickListener(this);
+        }
+
+        spCoffee1 = (Spinner) findViewById(R.id.spCoffee1);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spList);
+        spCoffee1.setAdapter(adapter1);
+        spCoffee1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spCoffee2 = (Spinner) findViewById(R.id.spCoffee2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spList2);
+        spCoffee2.setAdapter(adapter2);
+        spCoffee2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spCoffee3 = (Spinner) findViewById(R.id.spCoffee3);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spList3);
+        spCoffee3.setAdapter(adapter3);
+        spCoffee3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position==1)
+                    startActivity(new Intent(Floor2.this,MainActivity.class));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        for (int i = 0; i < btn.length; i++) {
+            if (btn[i] == v)
+                if(state[i]==false) {
+                    btn[i].setBackgroundColor(Color.RED);
+                    btn[i].setText("full");
+                    state[i] = true;
+                } else{
+                    state[i] = false;
+                    btn[i].setBackgroundColor(Color.GREEN);
+                    btn[i].setText("empty");
+                }
+        }
+        for (int i = 0; i < btnp.length; i++) {
+            if (btnp[i] == v)
+                if(state[i]==false) {
+                    btnp[i].setBackgroundColor(Color.RED);
+                    btnp[i].setText("full");
+                    state[i] = true;
+                } else{
+                    state[i] = false;
+                    btnp[i].setBackgroundColor(Color.BLUE);//getResources().getColor(R.color.purple)
+                    btnp[i].setText("empty");
+
+                }
+        }
+        for (int i = 0; i < btnb.length; i++) {
+
+            if (btnb[i] == v)
+                if(state[i]==false) {
+                    btnb[i].setBackgroundColor(Color.RED);
+                    btnb[i].setText("full");
+                    state[i] = true;
+                } else{
+                    state[i] = false;
+                    btnb[i].setBackgroundColor(Color.BLUE);
+                    btnb[i].setText("empty");
+                }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        AlertDialog.Builder Builder = new AlertDialog.Builder(this);
+        Builder.setTitle("About");
+        Builder.setMessage("our application is to let you win more time spending with friends and familys insted searching for parking places");
+
+        Builder.setPositiveButton("Done",new DialogInterface.OnClickListener(){
+
+            public void onClick(DialogInterface dialog,int which){
+               // Toast.makeText(getApplicationContext(),"Ok", Toast.LENGTH_LONG).show();
+            }
+
+
+        });
+        AlertDialog ad=Builder.create();
+        ad.show();
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+

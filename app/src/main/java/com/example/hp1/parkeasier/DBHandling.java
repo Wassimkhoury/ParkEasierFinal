9ceb78fabd61473427d6a4c903e11b185278623a
wassimkhoury <razan.kedan2000@gmail.com>
2018-02-15 11:10:31 +0200

    aa

--- app/src/main/java/com/example/hp1/parkeasier/DBHandling.java
+++ app/src/main/java/com/example/hp1/parkeasier/DBHandling.java
@@ -1,9 +1,13 @@
 package com.example.hp1.parkeasier;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * Created by Hp1 on 25/01/2018.
@@ -19,7 +23,7 @@ public class DBHandling extends SQLiteOpenHelper{
     public static final String DATABASE_NAME = "rate.db";
 
     //data table name
-    public static final String TABLE_ALC = "rate";
+    public static final String TABLE_RATE = "rate";
 
     //column names
     public static final String COL_ID ="id";
@@ -37,17 +41,17 @@ public class DBHandling extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-String query = " CREATE TABLE " + TABLE_ALC
+String query = " CREATE TABLE " + TABLE_RATE
         + "(" + COL_ID + " INTGER PRIMARY KEY AUTOINCREMENT, "
-        + COL_CARNUM + " INTEGER "
-        + COL_DATE + " TEXT "
-        + COL_RATE + " REAL "
-        + COL_PARKINPNUM + " INTEGER "
+        + COL_CARNUM + " TEXT, "
+        + COL_DATE + " TEXT, "
+        + COL_RATE + " REAL, "
+        + COL_PARKINPNUM + " INTEGER, "
         + COL_FLOORNUM + " INTEGER "
         +");";
     Log.d("QUERY",query);
         try {
-            db.execSQL("DROP TABLE IF EXISTS " + TABLE_ALC);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_RATE);
             db.execSQL(query);
         }catch (Exception e){
             e.printStackTrace();
@@ -57,8 +61,58 @@ String query = " CREATE TABLE " + TABLE_ALC
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ALC);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_RATE);
         onCreate(db);
 
     }
+    //add user to the database method
+    public void addRate(Rate rate){
+        //create ContentValue containning values to be inserted/updated in database in this case only on column
+        ContentValues values = new ContentValues();
+
+        values.put(COL_CARNUM, rate.getCarnum());
+        values.put(COL_DATE, rate.getDate());
+        values.put(COL_RATE, rate.getRate());
+        values.put(COL_PARKINPNUM, rate.getParkingnum());
+        values.put(COL_FLOORNUM, rate.getFloornum());
+
+        //create SQLiteDatabase object to enable writing/reading in database
+        SQLiteDatabase db = getWritableDatabase();
+        long id = db.insert(TABLE_RATE, null, values);
+        rate.setId(id);//update the user ID according to the auto incremented id in the DB
+
+        //logging for debugging purposes
+        Log.d("ID ", "Category id: "+id+" added to DB");
+
+        //close connection to database
+        db.close();
+    }
+    //search data from DB
+    public ArrayList<Rate> getData(){
+        String[] r = new String[6];
+        int[] col = new int[6];
+        String query = "SELECT * FROM "+ TABLE_RATE ;//+ " WHERE " + COL_USERNAME+"='"+username+"'";
+
+        ArrayList<Rate> rates= new ArrayList<Rate>();
+        SQLiteDatabase db = getWritableDatabase();
+        Cursor c = db.rawQuery(query, null);
+        c.moveToFirst();
+
+        col[0]=c.getColumnIndex(COL_ID);
+        col[1]=c.getColumnIndex(COL_CARNUM);
+        col[2]=c.getColumnIndex(COL_DATE);
+        col[3]=c.getColumnIndex(COL_RATE);
+        col[4]=c.getColumnIndex(COL_PARKINPNUM);
+        col[5]=c.getColumnIndex(COL_FLOORNUM);
+
+        while(!c.isAfterLast()){
+            for(int i=0;i<col.length;i++){
+                r[i]=c.getString(col[i]);
+            }
+            rates.add(new Rate(r[3],r[2],r[1],Long.parseLong(r[0]),double.class,));
+            c.moveToNext();
+        }
+        return alc;
+    }
+
 }
--- app/src/main/java/com/example/hp1/parkeasier/Rate.java
+++ app/src/main/java/com/example/hp1/parkeasier/Rate.java
@@ -5,4 +5,67 @@ package com.example.hp1.parkeasier;
  */
 
 public class Rate {
+    private  long id;
+    private String carnum;
+    private String date;
+    private double rate;
+    private int parkingnum;
+    private int floornum;
+
+    public Rate(long id, String carnum, String date, double rate, int parkingnum, int floornum) {
+        this.id = id;
+        this.carnum = carnum;
+        this.date = date;
+        this.rate = rate;
+        this.parkingnum = parkingnum;
+        this.floornum = floornum;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCarnum() {
+        return carnum;
+    }
+
+    public void setCarnum(String carnum) {
+        this.carnum = carnum;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public int getParkingnum() {
+        return parkingnum;
+    }
+
+    public void setParkingnum(int parkingnum) {
+        this.parkingnum = parkingnum;
+    }
+
+    public int getFloornum() {
+        return floornum;
+    }
+
+    public void setFloornum(int floornum) {
+        this.floornum = floornum;
+    }
 }
